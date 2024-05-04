46b0b437890adc106993a379f4550222bedac081
Ben Meehan <benmeehan111@gmail.com>
2024-05-04 12:24:27 +0530

    pass 1st stage

--- app/server.go
+++ app/server.go
@@ -10,12 +10,9 @@ import (
 func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	fmt.Println("Client connected from", conn.RemoteAddr())
-
 	reader := bufio.NewReader(conn)
 
 	for {
-		// Read the command from the client
 		_, err := reader.ReadString('\n')
 		if err != nil {
 			fmt.Println("Error reading command:", err)
