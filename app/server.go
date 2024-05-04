31f27d3f495655e0180e3f6e6e4e305634db8744
Ben Meehan <benmeehan111@gmail.com>
2024-05-04 12:15:23 +0530

    pass 1st stage

--- app/server.go
+++ app/server.go
@@ -6,15 +6,31 @@ import (
 	"os"
 )
 
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	response := "+PONG\r\n"
+	conn.Write([]byte(response))
+}
+
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	defer l.Close()
+
+	fmt.Println("Server listening on port 6379")
+
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go handleConnection(conn)
 	}
 }
