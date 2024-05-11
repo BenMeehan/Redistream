816c26ee570e102081ff403123aa30ae6a8fcd81
Ben Meehan <benmeehan111@gmail.com>
2024-05-11 15:14:48 +0530

    run tests

--- app/server.go
+++ app/server.go
@@ -133,7 +133,7 @@ func main() {
 	fmt.Println("Server listening on port", port)
 
 	if isReplica {
-		go ConnectToMasterHandshake(masterHost, masterPort)
+		ConnectToMasterHandshake(masterHost, masterPort)
 	}
 
 	for {
