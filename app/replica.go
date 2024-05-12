cd8d801add00e0e99dc30f73051d3904e1c6870e
Ben Meehan <benmeehan111@gmail.com>
2024-05-12 15:37:05 +0530

    run tests

--- app/replica.go
+++ app/replica.go
@@ -62,8 +62,6 @@ func (srv *serverState) replicaHandshake() {
 	}
 
 	go srv.handlePropagation(reader, masterConn)
-
-	srv.requestAcknowledgement()
 }
 
 var emptyRDB = []byte("524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2")
